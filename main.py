27658c9fc04d142cff08abd97c0d31efe8475490
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2023-10-17 06:09:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Explicit conversion
+a = "12"
+b = "23"
+c = ""
+print(int(a) + int(b))
+print(float(a) + float(b))
+print(str(a) + str(b))
+print(bool(a) + bool(b))
+print(a + b)
+print(bool(c))
+
+# Implicit Type conversion
+a = 2.453
+b = 30
+print(a+b)
